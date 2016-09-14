306f60f1d1d4649ec90cad9d83b82bd339efb59f
Gorapower <a.alexis.gora@gmail.com>
2016-09-13 17:20:11 -0700

    Cases for Demo wroking
    Add some cases for the demo

--- src/guindos/Guindos.java
+++ src/guindos/Guindos.java
@@ -5,6 +5,10 @@
  */
 package guindos;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 /**
  *
@@ -12,34 +16,57 @@ import java.util.ArrayList;
  */
 public class Guindos {
 
-    static ArrayList<String> data = new ArrayList<String>();
+    static ArrayList<Integer> data = new ArrayList<Integer>();
     static StringBuffer output = new StringBuffer();
     static int pairsToRead;
     static StringBuffer message = new StringBuffer();
     static int temp;
+    static String path = "fib.chop";
+    static String tempString;
+    static int bin;
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        //Test
-        
-        data.add("E1");
-        data.add("11");
-        data.add("21");
-        data.add("31");
-        data.add("41");
-        data.add("51");
-        data.add("AD");
-        data.add("F1");
-        pairsToRead = 8;
-        output.append(ReadString(data,pairsToRead));
-        System.out.print(output);
-        //End test
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+//        //Test
+//        
+//        data.add("E1");
+//        data.add("11");
+//        data.add("21");
+//        data.add("31");
+//        data.add("41");
+//        data.add("51");
+//        data.add("AD");
+//        data.add("F1");
+//        pairsToRead = 8;
+//        output.append(ReadString(data,pairsToRead));
+//        System.out.print(output);
+//        //End test
         
         //read file
-        
+        FileReader fr = new FileReader(path);
+        BufferedReader textReader = new BufferedReader(fr);
+        String line;
+        while((line = textReader.readLine())!=null)
+        {
+            line = line.replace(" ","");
+            for(int i=0;i <line.length();i++){
+                tempString = line.substring(i, (i)+1);
+                data.add((int)tempString.charAt(0));
+            }
+        }
+//        System.out.print("Test\n");
+//        for(int i=0;i<data.size();i++){
+//            System.out.print(data.get(i)+ "\n");
+//        }
+
         //end read file
+        
+        
+        for (int i=0;i<14;i++){
+            data.remove(0);
+        }
         
         //translate data -- comment this area to compile for now
         for(int i=0; i<data.size();){
@@ -47,17 +74,16 @@ public class Guindos {
             
             //end read first data
             
-            temp = Integer.parseInt(data.get(0), 16);
-            data.remove(0);
-            
             /* Use this line of code in the cases that you need to read a String
             output.append(ReadString(data,pairsToRead));
             */
-            
+            temp = data.get(0);
+            data.remove(0);
             // init cases
             switch(temp){
                 case 0:
                 //write to file "HALT"
+                    message.append("halt\n");
                     break;
                 case 1:
                 //write to file "PRTCR"
@@ -67,6 +93,11 @@ public class Guindos {
                     break;
                 case 3:
                 //write to file "PRTI"
+                    temp = readByte(data,2);
+                    message.append("prti  var");
+                    temp = temp/4;
+                    message.append(temp);
+                    message.append("\n");
                     break;
                 case 4:
                 //write to file "PRTF"
@@ -94,9 +125,14 @@ public class Guindos {
                     break;
                 case 12:
                 //write to file "PUSHAI"
+                    temp = readByte(data,2);
+                    message.append("pushi  var");
+                    temp = temp/4;
+                    message.append(temp);
+                    message.append("\n");
                     break;
                 case 13:
-                //write to file "PUSHAF"
+                //write to file "PUSHI"
                     break;
                 case 14:
                 //write to file "PUSHAD"
@@ -126,7 +162,11 @@ public class Guindos {
                 //write to file "POPC"
                     break;
                 case 23:
-                //write to file "POPI"
+                //write to file "pushki"
+                    temp = readByte(data,4);
+                    message.append("pushki ");
+                    message.append(temp);
+                    message.append("\n");
                     break;
                 case 24:
                 //write to file "POPF"
@@ -138,10 +178,15 @@ public class Guindos {
                 //write to file "POPS"
                     break;
                 case 27:
-                //write to file "POPX"
+                //write to file "popc"
                     break;
                 case 28:
-                 //write to file "POPAI"
+                 //write to file "POPI"
+                    temp = readByte(data,2);
+                    message.append("popi  var");
+                    temp = temp/4;
+                    message.append(temp);
+                    message.append("\n");
                     break;
                 case 29:
                 //write to file "POPAF"
@@ -174,7 +219,12 @@ public class Guindos {
                 //write to file "RDAI"
                     break;
                 case 39:
-                //write to file "RDAF"
+                //write to file "RDI"
+                    temp = readByte(data,2);
+                    message.append("rdi var");
+                    temp = temp/4;
+                    message.append(temp);
+                    message.append("\n");
                     break;
                 case 40:
                 //write to file "RDAD"
@@ -201,7 +251,9 @@ public class Guindos {
                 //write to file "JMPLT"
                     break;
                 case 48:
-                //write to file "JMPLE"
+                //write to file "JMP"
+                    message.append("jmp  loop\n");
+                    data.remove(0);
                     break;
                 case 49:
                 //write to file "STX"
@@ -216,7 +268,9 @@ public class Guindos {
                 //write to file "DEC"
                     break;
                 case 53:
-                //write to file "ADD"
+                //write to file "JPML"
+                    message.append("jmpl fin\n");
+                    data.remove(0);
                     break;
                 case 54:
                 //write to file "SUB"
@@ -228,11 +282,37 @@ public class Guindos {
                 //write to file "DIV"
                     break;
                 case 57:
-                //write to file "MOD"
+                //write to file "INC"
+                    temp = readByte(data,2);
+                    message.append("inc  var");
+                    temp = temp/4;
+                    message.append(temp);
+                    message.append("\n");
                     break;
                 case 58:
                 //write to file "CMP"
                     break;
+                case 59:
+                //write to file "ADD"
+                    message.append("add\n");
+                    break;
+                case 60:
+                //write to file "CMP"
+                    break;
+                case 61:
+                //write to file "CMP"
+                    break;
+                case 62:
+                //write to file "CMP"
+                    break;
+                case 63:
+                //write to file "CMP"
+                    break;
+                case 64:
+                //write to file "CMP"
+                    message.append("cmp\n");
+                    break;
+                                        
                 default:
                     break;
             }
@@ -243,7 +323,7 @@ public class Guindos {
         //end translate data -- commnet to here for compile
         
         //write to file
-        
+        System.out.print(message);
         //end write to file
     }
 
@@ -255,6 +335,30 @@ public class Guindos {
             data.remove(0);
         }
         return message;
+    }
+
+    private static int readByte(ArrayList<Integer> data, int i) {
+        temp=0;
+        if(i==4){
+            temp= data.get(0)*256*256*256;
+            data.remove(0);
+            i--;
+        }
+        if(i==3){
+            temp= data.get(0)*256*256;
+            data.remove(0);
+            i--;
+        }
+        if(i==2){
+            temp+= data.get(0)*256;
+            data.remove(0);
+            i--;
+        }
+        if(i==1){
+            temp+= data.get(0);
+            data.remove(0);
+        }
+        return temp;
     }
     
 }
